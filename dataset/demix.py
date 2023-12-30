1d74ee04b3ed01dc1a18929e9eecd58675b1b197
0417keito <0417keito@gmail.com>
2023-12-31 01:40:28 +0900

    add demix function

---
+++ dataset/demix.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+import torch
+
+from pathlib import Path
+from typing import List, Union
+
+
+def demix(paths: List[Path], demix_dir: Path, device: Union[str, torch.device]):
+  """Demixes the audio file into its sources."""
+  todos = []
+  demix_paths = []
+  for path in paths:
+    out_dir = demix_dir / 'htdemucs' / path.stem
+    demix_paths.append(out_dir)
+    if out_dir.is_dir():
+      if (
+        (out_dir / 'bass.wav').is_file() and
+        (out_dir / 'drums.wav').is_file() and
+        (out_dir / 'other.wav').is_file() and
+        (out_dir / 'vocals.wav').is_file()
+      ):
+        continue
+    todos.append(path)
+
+  existing = len(paths) - len(todos)
+  print(f'=> Found {existing} tracks already demixed, {len(todos)} to demix.')
+
+  if todos:
+    subprocess.run(
+      [
+        sys.executable, '-m', 'demucs.separate',
+        '--out', demix_dir.as_posix(),
+        '--name', 'htdemucs',
+        '--device', str(device),
+        *[path.as_posix() for path in todos],
+      ],
+      check=True,
+    )
+
+  return demix_paths
+
+def find_audio_files(directory: Path):
+    return list(directory.rglob('*.mp3'))  
+
+if __name__ == '__main__':
+    dataset_dir = '/home/keito/data/audios'
+    audio_dir = Path(dataset_dir)
+    audio_paths = find_audio_files(audio_dir)
+    demixed_paths = demix(audio_paths, audio_dir, "cuda") 
+    print('demixed_paths', demixed_paths)
