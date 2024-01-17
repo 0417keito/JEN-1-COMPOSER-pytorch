bc033fe1af19301045cd46afdc00d958f8127e78
0417keito <0417keito@gmail.com>
2024-01-17 17:06:02 +0900

    Change to stochastic selection of curriculum stages

--- utils/curriculum_scheduler.py
+++ utils/curriculum_scheduler.py
@@ -1,29 +1,32 @@
-class CurriculumScheduler:
-    def __init__(self, total_epochs, stage_ratios):
-        self.total_epochs = total_epochs
-        self.stage_ratios = stage_ratios
-        self.curriculum_stages = len(stage_ratios)
-        self.stage_epochs = self.calculate_stage_epochs()
-        self.current_stage = 1
-    
-    def calculate_stage_epochs(self):
-        stage_epochs = []
-        for ratio in self.stage_ratios:
-            epochs = round(self.total_epochs * ratio)
-            stage_epochs.append(epochs)
-        return stage_epochs
-    
-    def update_stage(self):
-        if self.current_stage < self.curriculum_stages:
-            self.current_stage += 1
-    
-    def get_current_stage_epochs(self):
-        start_epoch = sum(self.stage_epochs[:self.current_stage - 1])
-        end_epoch = start_epoch + self.stage_epochs[self.current_stage -1]
-        return start_epoch, end_epoch
-    
+import numpy as np
+import random
+
+def curriculum_scheduler(current_step, total_steps):
+    """
+    Returns the selected class based on the current step using
+    cosine and sine functions for a non-linear change in probabilities.
+
+    :param current_step: The current step number.
+    :param total_steps: Total number of steps.
+    :return: The selected class.
+    """
+    # Adjusting the phase and scale of the cosine and sine functions
+    class1_prob = (np.cos(np.pi * current_step / total_steps) + 1) / 2
+    class2_prob = np.sin(np.pi * current_step / total_steps)
+    class3_prob = (np.cos(np.pi * current_step / total_steps - np.pi) + 1) / 2
+
+    # Ensure total probability is 1
+    total_prob = class1_prob + class2_prob + class3_prob
+    class1_prob = class1_prob / total_prob
+    class2_prob = class2_prob / total_prob
+    class3_prob = class3_prob / total_prob
+
+    # Select a class based on the probabilities
+    selected_class = random.choices([1, 2, 3], weights=[class1_prob, class2_prob, class3_prob], k=1)[0]
+    return selected_class
+
 if __name__ == '__main__':
-    total_epochs = 100
-    stage_ratios = [0.1, 0.2, 0.3, 0.2, 0.2]
     
-    curriculum_scheduler = CurriculumScheduler(total_epochs, stage_ratios)
+    # Example usage
+    total_steps = 10
+    selected_class = [curriculum_scheduler(step, total_steps) for step in range(total_steps)]
